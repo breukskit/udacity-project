df3479f5cb61d94b40df05e4229a3cc16f36d11a
Daniel Forstén <daniel.forsten@hotmail.com>
2020-09-30 11:50:07 +0200

    nav submenu complete

--- src/app/components/nav/menus.ts
+++ src/app/components/nav/menus.ts
@@ -36,6 +36,59 @@ export const ARTIFICIAL_INTELLIGENCE = [
   'AI for Business Leaders',
 ];
 
+export const CLOUD_COMPUTING = [
+  'Developer for Microsoft Azure',
+  'DevOps Engineer for Microsoft Azure',
+  'AWS Cloud Architext',
+  'Cloud Developer',
+  'Cloud DevOps Engineer',
+];
+
+export const DATA_SCIENCE = [
+  'Data Product Manager',
+  'SQL',
+  'Data Analyst',
+  'Predictive Analytics for Business',
+  'Data Scientist',
+  'Data Engineer',
+  'Marketing Analytics',
+  'Business Analytics',
+  'Programming for Data Science with Python',
+  'Programming for Data Science with R',
+  'Data Visualization',
+  'Data Structures and Algorithms',
+  'Data Streaming',
+  'Data Science for Business Leaders',
+];
+
+export const BUSINESS = [
+  'Agile Software Development',
+  'Data Science for Business Leaders',
+  'Monetization Strategy',
+  'Growth Product Manager',
+  'Data Product Manager',
+  'SQL',
+  'Product Manager',
+  'Predictive Analytics for Business',
+  'Digital Marketing',
+  'Marketing Analytics',
+  'AI for Business Leaders',
+  'AI Product Manager',
+  'Business Analytics',
+  'Data Visualization',
+  'UX Designer',
+];
+
+export const AUTONOMOUS_SYSTEMS = [
+  'Self Driving Car Engineer',
+  'Intro to Self-Driving Cars',
+  'Robotics Software Engineer',
+  'C++',
+  'Data Structures and Algorithms',
+  'Flying Car and Autonomous Flight Engineer',
+  'Sensor Fusion Engineer',
+];
+
 export const PROGRAMMING = [
   'Programming and development',
   'Artificial intelligence',
--- src/app/components/nav/programs-dropdown/programs-dropdown.component.ts
+++ src/app/components/nav/programs-dropdown/programs-dropdown.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { PROGRAMMING_AND_DEVELOPMENT, ARTIFICIAL_INTELLIGENCE } from '../menus';
+import {
+  PROGRAMMING_AND_DEVELOPMENT,
+  ARTIFICIAL_INTELLIGENCE,
+  CLOUD_COMPUTING,
+  DATA_SCIENCE,
+  BUSINESS,
+  AUTONOMOUS_SYSTEMS,
+} from '../menus';
 
 @Component({
   selector: 'app-programs-dropdown',
@@ -7,18 +14,14 @@ import { PROGRAMMING_AND_DEVELOPMENT, ARTIFICIAL_INTELLIGENCE } from '../menus';
   styleUrls: ['./programs-dropdown.component.scss'],
 })
 export class ProgramsDropdownComponent implements OnInit {
-  menuTarget = '';
   constructor() {}
-
-  over(target: string) {
-    this.menuTarget = target;
-  }
-  out() {
-    this.menuTarget = '';
-  }
 
   programs = PROGRAMMING_AND_DEVELOPMENT;
   artificialIntelligence = ARTIFICIAL_INTELLIGENCE;
+  cloudComputing = CLOUD_COMPUTING;
+  dataScience = DATA_SCIENCE;
+  business = BUSINESS;
+  autonomousSystems = AUTONOMOUS_SYSTEMS;
 
   ngOnInit(): void {}
 }
