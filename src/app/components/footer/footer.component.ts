607d8ce07fd8c73259d06224764d410a1a1fdff2
Daniel Forstén <daniel.forsten@hotmail.com>
2020-10-06 15:00:33 +0200

    before animations

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import {
   FEATURED_PROGRAMS,
   ONLY_AT_UDACITY,
@@ -9,14 +9,43 @@ import {
   ENTERPRISE,
   SUPPORT,
 } from './Lists';
+import { State } from '../../../store/reducers';
+import { selectSize } from '../../../store/selectors';
+import { Store, select } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { faPlus, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent implements OnInit {
-  constructor() {}
+export class FooterComponent implements OnInit, OnDestroy {
+  sub: Subscription;
+  viewPort: string;
+
+  faPlus = faPlus;
+  faTimes = faTimes;
+  toggleMenus(menu: string) {
+    this.menus.forEach((item) => {
+      if (item.id === menu) {
+        item.show = !item.show;
+      }
+    });
+  }
+
+  menus = [
+    { show: false, id: 'featuredPrograms' },
+    { show: false, id: 'onlyAtUdacity' },
+    { show: false, id: 'udacitySchools' },
+    { show: false, id: 'resources' },
+    { show: false, id: 'programs' },
+    { show: false, id: 'company' },
+    { show: false, id: 'enterprise' },
+    { show: false, id: 'support' },
+  ];
+
+  constructor(private store: Store<State>) {}
 
   featuredPrograms = FEATURED_PROGRAMS;
   onlyAtUdacity = ONLY_AT_UDACITY;
@@ -27,5 +56,12 @@ export class FooterComponent implements OnInit {
   enterprise = ENTERPRISE;
   support = SUPPORT;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.sub = this.store
+      .pipe(select(selectSize))
+      .subscribe((x) => (this.viewPort = x));
+  }
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
